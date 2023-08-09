0702cef68bdb637291f9a48998b1ec912ad7f01a
Pratyaksh Aggarwal <pratyaksh2002@gmail.com>
2023-08-09 11:58:04 +0530

    added new files

---
+++ Leetcode/2616_Minimize_the_Maximum_Difference_of_Pairs.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isPossible(vector<int> &nums, int p, int diff){
+        int n = nums.size();
+        for(int i = 1; i < n; i++){
+            if(nums[i]-nums[i-1] <= diff){
+                p--;
+                i++;
+            }
+        }
+        return p <= 0;
+    }
+
+    int minimizeMax(vector<int>& nums, int p) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int left = 0, right = nums[n-1]-nums[0];
+        int ans = right;
+
+        while(left <= right){
+            int mid = left + (right-left)/2;
+            if(isPossible(nums, p, mid)){
+                ans = mid;
+                right = mid - 1;
+            }
+            else left = mid + 1;
+        }
+        return ans;
+    }
+};
